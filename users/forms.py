49360d3f6c89335354dca3e1d1c09bf3f0103b16
Dmitry_Bezgubov <dmitrij-bezgubov@yandex.ru>
2025-03-03 11:21:31 +0500

    step 1 html and views login / register

--- users/forms.py
+++ users/forms.py
@@ -1,23 +1,21 @@
 from django import forms
-from .models import CustomUser
 from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
+class UserRegisterForm(UserCreationForm):
+    """
+    Форма для регистрации пользователя.
+    """
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    )
 
-class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ('email', 'password1', 'password2')
-
-class CustomUserCreationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'first_name', 'last_name']
-
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ['avatar', 'phone', 'country']
-
+        fields = ['email', 'password1', 'password2']
